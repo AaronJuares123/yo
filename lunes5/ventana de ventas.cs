111c0dab47b04c49df994a91a22689c2b356de69
Gerardo Barrientos <ge6975507@gmail.com>
2023-06-06 22:02:13 -0600

    listo

---
+++ lunes5/ventana de ventas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lunes5
+{
+    public partial class ventana_de_ventas : Form
+    {
+        public ventana_de_ventas()
+        {
+            InitializeComponent();
+        }
+    }
+}
