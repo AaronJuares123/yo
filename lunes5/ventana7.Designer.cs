f048ea0e0b402e3c050eb8d61baa35f6fcedc820
Gerardo Barrientos <ge6975507@gmail.com>
2023-06-09 15:47:23 -0600

    tyju

--- lunes5/ventana7.Designer.cs
+++ lunes5/ventana7.Designer.cs
@@ -309,7 +309,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(618, 69);
+            this.label6.Location = new System.Drawing.Point(616, 89);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(100, 24);
             this.label6.TabIndex = 18;
@@ -319,7 +319,7 @@
             // 
             this.textBox5.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox5.Location = new System.Drawing.Point(610, 34);
+            this.textBox5.Location = new System.Drawing.Point(606, 60);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(110, 26);
